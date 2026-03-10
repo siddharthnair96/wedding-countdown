69568f2a1bde29ace5e97482609b6dace34d2702
Siddharth Nair <siddharthnair96@gmail.com>
2026-03-10 14:11:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const targetDate = new Date("October 12, 2026 07:00:00").getTime();
+
+const timer = setInterval(function(){
+
+const now = new Date().getTime();
+
+const distance = targetDate - now;
+
+const days = Math.floor(distance / (1000*60*60*24));
+
+const hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+
+const minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
+
+const seconds = Math.floor((distance % (1000*60)) / 1000);
+
+document.getElementById("days").innerHTML = days;
+
+document.getElementById("hours").innerHTML = hours;
+
+document.getElementById("minutes").innerHTML = minutes;
+
+document.getElementById("seconds").innerHTML = seconds;
+
+if(distance < 0){
+
+clearInterval(timer);
+
+document.querySelector(".countdown").innerHTML="🎉 The Wedding Day Is Here";
+
+confetti({
+particleCount:200,
+spread:100
+});
+
+}
+
+},1000);
